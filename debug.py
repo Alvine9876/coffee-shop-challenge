d33f0a76c0583634f9aaf13cf882e5168c3936ac
Alvine Clive Munene <alvine.munene@student.moringaschool.com>
2025-05-20 12:08:19 +0300

    debug.py

--- debug.py
+++ debug.py
@@ -1,13 +1,10 @@
 from customer import Customer
 from coffee import Coffee
 
-
 alvine = Customer("Alvine")  
 latte = Coffee("Latte")  
 
-
 alvine.create_order(latte, 5.5)
-
 
 print(alvine.name)
 
